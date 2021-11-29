d754cc606f93134542d4c96813ae17a1d53a93ae
JoakimJohnsson <joakim.johnsson@su.se>
2021-11-29 08:07:03 +0100

    check for email and password login input

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,6 +7,8 @@ const Login = () => {
     // Error and validation handling
     const [showFormError, setShowFormError] = useState(false);
     const [formErrorMessage, setFormErrorMessage] = useState("");
+    const [emailValidated, setEmailValidated] = useState(false);
+    const [passwordValidated, setPasswordValidated] = useState(false);
 
     const emailRef = useRef();
     const passwordRef = useRef();
@@ -28,6 +30,19 @@ const Login = () => {
         }
     }
 
+    const handleEmailValidation = (e) => {
+        // Send true for success
+        e.target.value !== "" ? setEmailValidated(true) : setEmailValidated(false);
+    }
+
+    const handlePasswordValidation = (e) => {
+        e.target.value !== "" ? setPasswordValidated(true) : setPasswordValidated(false);
+    }
+
+    const enableSubmitButton = () => {
+        return emailValidated && passwordValidated;
+    }
+
     return (
         <div className={"w-100"}>
             <form onSubmit={handleSubmit} className={"sms-form"}>
@@ -35,17 +50,19 @@ const Login = () => {
                 <input id="input-email"
                        type="email"
                        ref={emailRef}
-                       className={"form-control mb-3"}
+                       onChange={(e) => handleEmailValidation(e)}
+                       className={emailValidated ? "form-control success mb-3" : "form-control mb-3"}
                        placeholder={"name@myplace.se"}
                        required/>
                 <label className={"form-label"} htmlFor="input-password">Password</label>
                 <input id="input-password"
                        type="password"
                        ref={passwordRef}
-                       className={"form-control mb-3"}
+                       onChange={(e) => handlePasswordValidation(e)}
+                       className={passwordValidated ? "form-control success mb-3" : "form-control mb-3"}
                        placeholder={"********"}
                        required/>
-                <button type="submit" className={"btn btn-primary"}>Log in</button>
+                <button type="submit" className={enableSubmitButton() === true ? "btn btn-primary" : "btn btn-primary disabled"}>Log in</button>
                 {showFormError ?
                     <p className={"alert alert-danger mt-3"} role={"alert"}>{formErrorMessage}</p>
                     :
