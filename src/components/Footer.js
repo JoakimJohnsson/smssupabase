8512953037fe1dfa2eae20274cddb063908ca913
JoakimJohnsson <joakim.johnsson@su.se>
2021-12-02 11:28:19 +0100

    refactoring

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
             <div className={"container"}>
                 <div className={"row"}>
                     <div className={"col-12 col-md-6 px-4 py-2 border-md-end"}>
-                        <p className={"fs-smaller text-"}>
+                        <p className={"fs-smaller"}>
                             This webb application contains images and information which is owned and copyrighted
                             by <a href="https://www.marvel.com">MARVEL Entertainment</a> and is used without permission
                             according to the fair use doctrin of the United States.
