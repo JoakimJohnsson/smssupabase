1d4b8c318bf4b99138cef0c94ae0bf231d9af2ea
JoakimJohnsson <joakim.johnsson@su.se>
2022-01-23 20:32:05 +0100

    add working offcanvas sidebar for mobile dashboard

--- src/components/dashboardComponents/Sidebar.js
+++ src/components/dashboardComponents/Sidebar.js
@@ -1,19 +1,20 @@
 import React from "react";
 import Offcanvas from "react-bootstrap/Offcanvas";
 import {LABELS_AND_HEADINGS} from "../../helpers/constants";
+import SidebarBody from "./SidebarBody";
 
 
 const Sidebar = ({isOpen, handleClick}) => {
 
     return (
         <div className={'d-none d-md-flex dashboard-sidebar'}>
+            <SidebarBody />
             <Offcanvas show={isOpen} onHide={handleClick}>
                 <Offcanvas.Header closeButton closeLabel={LABELS_AND_HEADINGS.CLOSE}>
                     <Offcanvas.Title>Offcanvas</Offcanvas.Title>
                 </Offcanvas.Header>
                 <Offcanvas.Body>
-                    Some text as placeholder. In real life you can have the elements you
-                    have chosen. Like, text, images, lists, etc.
+                    <SidebarBody />
                 </Offcanvas.Body>
             </Offcanvas>
         </div>
---
+++ src/components/dashboardComponents/SidebarBody.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+const SidebarBody = () => {
+
+    return (
+        <>
+            <p> samma content här</p>
+        </>
+    )
+}
+
+export default SidebarBody;
