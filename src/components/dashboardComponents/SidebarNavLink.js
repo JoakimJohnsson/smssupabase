774a160ca43668e1dc6a816b0e7a27d42c200785
JoakimJohnsson <joakim.johnsson@su.se>
2022-01-26 16:52:50 +0100

    refactoring

--- src/components/dashboardComponents/SidebarNavLink.js
+++ src/components/dashboardComponents/SidebarNavLink.js
@@ -5,7 +5,7 @@ const SidebarNavLink = ({eventKey, variant, Icon, text, handleClick}) => {
     const className = "nav-item";
     return (
         <Nav.Link eventKey={eventKey} className={className + ' ' + variant}>
-            <div onClick={handleClick} className={'d-flex align-items-center justify-content-sm-start justify-content-center flex-column flex-sm-row w-100'}>
+            <div onClick={handleClick} className={'d-flex align-items-center justify-content-center justify-content-sm-start justify-content-md-center justify-content-lg-start  flex-column flex-sm-row flex-md-column flex-lg-row w-100'}>
                 {Icon}
                 <span className={'sidebar-navlink-text'}>{text}</span>
             </div>
