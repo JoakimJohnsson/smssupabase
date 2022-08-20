11356f3b9bddfac4c0eb3c7d1787fd66353a774f
JoakimJohnsson <joakim.johnsson@su.se>
2022-08-20 09:54:48 +0200

    fix - close sidebar when click on link

--- src/components/dashboardComponents/SidebarNavLink.js
+++ src/components/dashboardComponents/SidebarNavLink.js
@@ -1,10 +1,10 @@
 import React from "react";
 import {NavLink} from "react-router-dom";
 
-export const SidebarNavLink = ({route, variant, icon, text}) => {
+export const SidebarNavLink = ({route, variant, icon, text, handleClick}) => {
     const className = "nav-item nav-link";
     return (
-        <NavLink to={route} className={className + ' ' + variant}>
+        <NavLink to={route} className={className + ' ' + variant} onClick={handleClick}>
             <div
                 className={'d-flex align-items-center justify-content-center justify-content-sm-start justify-content-md-center ' +
                     'justify-content-lg-start  flex-column flex-sm-row flex-md-column flex-lg-row w-100'}>
