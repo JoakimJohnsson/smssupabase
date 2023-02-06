b1f899525a36d1015e2f81c675cc1af5ab83614d
JoakimJohnsson <joakim.johnsson@su.se>
2023-02-06 14:40:07 +0100

    test

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -167,7 +167,7 @@ export const LABELS_AND_HEADINGS = {
     UPLOAD_NEW_IMAGE: "Ladda upp en ny bild",
     UPLOAD_IMAGE: "Ladda upp en bild",
     WEBSITE: "Webbplats",
-    WELCOME: "Välkommen till Svenska Marvelsamlares coola sida!",
+    WELCOME: "Välkommen till Svenska Marvelsamlare!",
     YEAR: "År",
     YEAR_DB: "År (year)"
 }
