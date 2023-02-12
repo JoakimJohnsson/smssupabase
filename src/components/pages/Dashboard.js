494007fbe90528a44a94436667a7bb2f156ed8b5
JoakimJohnsson <joakim.johnsson@su.se>
2023-02-12 09:08:16 +0100

    Sidebar and offcanvas menu fixes

--- src/components/pages/Dashboard.js
+++ src/components/pages/Dashboard.js
@@ -2,7 +2,7 @@ import React, {useState} from "react";
 import {Outlet} from "react-router-dom";
 import {LABELS_AND_HEADINGS} from "../../helpers/constants";
 import {Sidebar} from "../dashboard/Sidebar";
-import {faChevronsRight} from "@fortawesome/pro-regular-svg-icons";
+import {faChevronsRight, faTimes} from "@fortawesome/pro-regular-svg-icons";
 import {Icon} from "../icons";
 
 
@@ -13,18 +13,20 @@ export const Dashboard = () => {
         setIsOpen(!isOpen)
     }
 
+    const btnClass = isOpen ? "sidebar-btn open" : "sidebar-btn";
+
     return (
         <main className={"container-fluid main-container dashboard"}>
             <div className={"row"}>
                 <Sidebar isOpen={isOpen} handleClick={handleClick}/>
-                <div className={"p-0 d-block d-md-none "}>
+                <div className={"p-0 d-block d-sm-none "}>
                     <button
-                        className={"btn btn-sm btn-primary sms-icon-btn rounded-0 rounded-end mb-3 mt-3 ms-0 p-2 text-start"}
+                        className={btnClass}
                         onClick={handleClick}
                         aria-label={LABELS_AND_HEADINGS.MENU}
                         aria-controls={"sidebar-menu"}
                     >
-                        <Icon icon={faChevronsRight} className={"sms-icon--hovering me-0"}/>
+                        <Icon icon={isOpen ? faTimes : faChevronsRight} className={"sms-icon--hovering me-0"}/>
                     </button>
                 </div>
                 <div className={"col row-padding--main"}>
