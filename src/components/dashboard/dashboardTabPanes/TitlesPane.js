6a24edc3bbeeb2cfe9ef8bf5964b9f62b93c5cb3
JoakimJohnsson <joakim.johnsson@su.se>
2023-03-05 15:54:03 +0100

    Show titles for user

--- src/components/dashboard/dashboardTabPanes/TitlesPane.js
+++ src/components/dashboard/dashboardTabPanes/TitlesPane.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import {PANES} from "../../../helpers/constants";
+import {TitlesList} from "../../lists/titles/TitlesList";
+import {useAppContext} from "../../../context/AppContext";
+import {getTitlesForUser} from "../../../helpers/functions/serviceFunctions/titleFunctions";
+import {CustomSpinner} from "../../minis/CustomSpinner";
+
 
 export const TitlesPane = () => {
+
+    const [loading, setLoading] = useState(true);
+    const [titlesData, setTitlesData] = useState(null);
+    const {user} = useAppContext();
+
+    useEffect(() => {
+        getTitlesForUser(user.id, setTitlesData).then(() => setLoading(false));
+    }, [user.id])
+
     return (
-        <div>
+        <>
             <h1>{PANES.TITLES.NAME}</h1>
             <p className="lead mb-5">
                 {PANES.TITLES.NAME}
             </p>
-        </div>
+            {
+                loading ?
+                    <CustomSpinner size={"4x"}/>
+                    :
+                    <div className={"sms-section--light"}>
+                        <TitlesList titlesData={titlesData} showAdminInfo={false}/>
+                    </div>
+            }
+
+        </>
     )
 }
