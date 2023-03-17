297c10dbe3b60275feff3b722f48f2c4cee3effb
JoakimJohnsson <joakim.johnsson@su.se>
2023-03-17 12:17:54 +0100

    Update release information

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -40,30 +40,37 @@ export const Home = () => {
                     </div>
                 </div>
                 <div className={"row row-padding--secondary"}>
-                        <div className={"sms-dashboard-col"}>
-                            <div className={"sms-section--light"}>
-                                <h2>Senaste releasen</h2>
-                                {utilsData && <p>v{package_json.version} | {utilsData.release_date}</p>}
-                                <ul className={"list-group list-group-flush"}>
-                                    <li className={"list-group-item px-0"}>Lista titlar utgivna av förlaget på dess sida.</li>
-                                    <li className={"list-group-item px-0"}>Visa tooltip på knappar för att lägga till titel eller publikation till samlingen.</li>
-                                    <li className={"list-group-item px-0"}>Förbättrad responsivitet på verktygsknappar i listor.</li>
-                                    <li className={"list-group-item px-0"}>Snyggare och rakare laddsnurra.</li>
-                                    <li className={"list-group-item px-0"}>Användare kan nu lägga till titlar till samlingen.</li>
-                                    <li className={"list-group-item px-0"}>Visa information om releaser och kommande förbättringar på startsidan.</li>
-                                    <li className={"list-group-item px-0"}>Småfixar och andra förbättringar.</li>
-                                </ul>
-                            </div>
+                    <div className={"sms-dashboard-col"}>
+                        <div className={"sms-section--light"}>
+                            <h2>{LABELS_AND_HEADINGS.RELEASE_LATEST}</h2>
+                            {utilsData && <p>v{package_json.version} | {utilsData.release_date}</p>}
+                            <ul className={"list-group list-group-flush"}>
+                                <li className={"list-group-item px-0"}>Förberett funktionalitet för skickgradering (WIP).</li>
+                                <li className={"list-group-item px-0"}>Badges för skickgradering, marvelklubben, format och land.</li>
+                                <li className={"list-group-item px-0"}>Nya färger för olika badges.</li>
+                                <li className={"list-group-item px-0"}>Nytt titelkort.</li>
+                                <li className={"list-group-item px-0"}>Visa bild i listor istället för ikoner.</li>
+                                <li className={"list-group-item px-0"}>Möjlighet att bläddra till nästa / föregående publikation.</li>
+                            </ul>
                         </div>
-                        <div className={"sms-dashboard-col"}>
-                            <div className={"sms-section--light"}>
-                                <h2>Kommande release</h2>
-                                <ul className={"list-group list-group-flush"}>
-                                    <li className={"list-group-item px-0"}>Småfixar och förbättringar.</li>
-                                </ul>
-                            </div>
+                    </div>
+                    <div className={"sms-dashboard-col"}>
+                        <div className={"sms-section--light"}>
+                            <h2>{LABELS_AND_HEADINGS.RELEASE_LATEST}</h2>
+                            <ul className={"list-group list-group-flush"}>
+                                <li className={"list-group-item px-0"}>Lista titlar utgivna av förlaget på dess sida.</li>
+                                <li className={"list-group-item px-0"}>Visa tooltip på knappar för att lägga till titel eller publikation till
+                                    samlingen.
+                                </li>
+                                <li className={"list-group-item px-0"}>Förbättrad responsivitet på verktygsknappar i listor.</li>
+                                <li className={"list-group-item px-0"}>Snyggare och rakare laddsnurra.</li>
+                                <li className={"list-group-item px-0"}>Användare kan nu lägga till titlar till samlingen.</li>
+                                <li className={"list-group-item px-0"}>Visa information om releaser och kommande förbättringar på startsidan.</li>
+                                <li className={"list-group-item px-0"}>Småfixar och andra förbättringar.</li>
+                            </ul>
                         </div>
-                        </div>
+                    </div>
+                </div>
 
 
             </main>
