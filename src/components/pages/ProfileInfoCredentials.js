3cc71d8611f94e86d3f890ca63e6cacfde6c0064
JoakimJohnsson <joakim.johnsson@su.se>
2023-04-30 13:14:00 +0200

    Refactoring profile info edit

---
+++ src/components/pages/ProfileInfoCredentials.js
@@ -0,0 +1,76 @@
+import React from "react";
+import {LABELS_AND_HEADINGS, TEXTS} from "../../helpers/constants";
+import {KeyIcon, SendIcon} from "../icons";
+
+
+export const ProfileInfoCredentials = ({
+                                           newEmail,
+                                           setNewEmail,
+                                           confirmNewEmail,
+                                           setConfirmNewEmail,
+                                           handleChangeEmail,
+                                           message,
+                                           pwMessage,
+                                           newPassword,
+                                           setNewPassword,
+                                           confirmNewPassword,
+                                           setConfirmNewPassword,
+                                           handleChangePassword
+                                       }) => {
+
+    return (
+        <div>
+            <h2>{LABELS_AND_HEADINGS.SETTINGS_CREDENTIALS}</h2>
+            <p>{TEXTS.SETTINGS_CREDENTIALS}</p>
+            <div className={"mb-3"}>
+                <label className={"form-label"} htmlFor="input-email">{LABELS_AND_HEADINGS.NEW_EMAIL}</label>
+                <input id="input-email"
+                       type="email"
+                       onChange={(e) => setNewEmail(e.target.value)}
+                       className={"form-control mb-3"}
+                       placeholder={"name@myplace.se"}
+                       required/>
+                <label className={"form-label"} htmlFor="input-confirm-email">{LABELS_AND_HEADINGS.CONFIRM_NEW_EMAIL}</label>
+                <input id="input-confirm-email"
+                       type="email"
+                       onChange={(e) => setConfirmNewEmail(e.target.value)}
+                       className={"form-control"}
+                       placeholder={"name@myplace.se"}
+                       required/>
+                <div className={"form-text mb-3"}>{TEXTS.CHANGE_EMAIL_SEND_INFO}</div>
+                <button className={"btn btn-primary sms-btn"} onClick={() => handleChangeEmail()}
+                        disabled={newEmail === "" || newEmail !== confirmNewEmail}>
+                    <SendIcon className={"me-2"}/>{LABELS_AND_HEADINGS.SEND}
+                </button>
+                {message.show && <p className={`alert ${message.isError ? "alert-danger" : "alert-success"} mt-3`}
+                                    role={"alert"}>{message.text}</p>}
+            </div>
+            <div>
+                <label className={"form-label d-flex"}
+                       htmlFor="input-current-password">{LABELS_AND_HEADINGS.NEW_PASSWORD}</label>
+                <input id="input-current-password"
+                       type="password"
+                       onChange={(e) => setNewPassword(e.target.value)}
+                       className={"form-control mb-3"}
+                       placeholder={"********"}
+                       required/>
+                <label className={"form-label d-flex"}
+                       htmlFor="input-new-password">{LABELS_AND_HEADINGS.PASSWORD_CONFIRM}</label>
+                <input id="input-new-password"
+                       type="password"
+                       onChange={(e) => setConfirmNewPassword(e.target.value)}
+                       className={"form-control"}
+                       placeholder={"********"}
+                       required/>
+                <div className={"form-text mb-3"}>{TEXTS.CHANGE_PASSWORD_INFO}</div>
+                <button className={"btn btn-danger sms-btn"} onClick={() => handleChangePassword()}
+                        disabled={confirmNewPassword === "" || newPassword !== confirmNewPassword}>
+                    <KeyIcon className={"me-2"}/>{LABELS_AND_HEADINGS.RESET_PASSWORD}
+                </button>
+                {pwMessage.show && <p className={`alert ${pwMessage.isError ? "alert-danger" : "alert-success"} mt-3`}
+                                      role={"alert"}>{pwMessage.text}</p>}
+            </div>
+
+        </div>
+    )
+}
--- src/components/pages/ProfileInfoEdit.js
+++ src/components/pages/ProfileInfoEdit.js
@@ -24,57 +24,55 @@ export const ProfileInfoEdit = ({profile, setProfile, newProfile, setNewProfile}
     }
 
     return (
-        <div className={"sms-dashboard-col"}>
-            <div className={"sms-section--light"}>
-                <h2>{LABELS_AND_HEADINGS.EDIT_INFORMATION}</h2>
-                <label className={"form-label"} htmlFor="firstname">{LABELS_AND_HEADINGS.FIRST_NAME}</label>
-                <input
-                    id="firstname"
-                    name="firstname"
-                    className={CLASSES.FORM_INPUT_DEFAULT}
-                    type="text"
-                    value={newProfile.firstname || ""}
-                    onChange={(e) => handleChange(newProfile, setNewProfile, e.target.name, e.target.value)}
-                    disabled={!edit}
-                />
-                <label className={"form-label"} htmlFor="lastname">{LABELS_AND_HEADINGS.LAST_NAME}</label>
-                <input
-                    id="lastname"
-                    name="lastname"
-                    className={CLASSES.FORM_INPUT_DEFAULT}
-                    type="text"
-                    value={newProfile.lastname || ""}
-                    onChange={(e) => handleChange(newProfile, setNewProfile, e.target.name, e.target.value)}
-                    disabled={!edit}
-                />
-                <label className={"form-label"} htmlFor="website">{LABELS_AND_HEADINGS.WEBSITE}</label>
-                <input
-                    id="website"
-                    name="website"
-                    className={CLASSES.FORM_INPUT_DEFAULT}
-                    type="text"
-                    value={newProfile.website || ""}
-                    onChange={(e) => handleChange(newProfile, setNewProfile, e.target.name, e.target.value)}
-                    disabled={!edit}
-                />
-                {
-                    edit ?
-                        <>
-                            <button onClick={handleSubmit} className={"btn btn-primary sms-btn"}>
-                                {LABELS_AND_HEADINGS.SAVE}
-                            </button>
-                            <button className={"btn btn-secondary sms-btn"} onClick={handleAbort}>
-                                {LABELS_AND_HEADINGS.ABORT}
-                            </button>
-                        </>
-                        :
-                        <>
-                            <button onClick={() => setSearchParams({edit: true})} className={"btn btn-primary sms-btn"}>
-                                {LABELS_AND_HEADINGS.EDIT}
-                            </button>
-                        </>
-                }
-            </div>
+        <div className={"mb-4"}>
+            <h2>{LABELS_AND_HEADINGS.EDIT_INFORMATION}</h2>
+            <label className={"form-label"} htmlFor="firstname">{LABELS_AND_HEADINGS.FIRST_NAME}</label>
+            <input
+                id="firstname"
+                name="firstname"
+                className={CLASSES.FORM_INPUT_DEFAULT}
+                type="text"
+                value={newProfile.firstname || ""}
+                onChange={(e) => handleChange(newProfile, setNewProfile, e.target.name, e.target.value)}
+                disabled={!edit}
+            />
+            <label className={"form-label"} htmlFor="lastname">{LABELS_AND_HEADINGS.LAST_NAME}</label>
+            <input
+                id="lastname"
+                name="lastname"
+                className={CLASSES.FORM_INPUT_DEFAULT}
+                type="text"
+                value={newProfile.lastname || ""}
+                onChange={(e) => handleChange(newProfile, setNewProfile, e.target.name, e.target.value)}
+                disabled={!edit}
+            />
+            <label className={"form-label"} htmlFor="website">{LABELS_AND_HEADINGS.WEBSITE}</label>
+            <input
+                id="website"
+                name="website"
+                className={CLASSES.FORM_INPUT_DEFAULT}
+                type="text"
+                value={newProfile.website || ""}
+                onChange={(e) => handleChange(newProfile, setNewProfile, e.target.name, e.target.value)}
+                disabled={!edit}
+            />
+            {
+                edit ?
+                    <>
+                        <button onClick={handleSubmit} className={"btn btn-primary sms-btn"}>
+                            {LABELS_AND_HEADINGS.SAVE}
+                        </button>
+                        <button className={"btn btn-secondary sms-btn"} onClick={handleAbort}>
+                            {LABELS_AND_HEADINGS.ABORT}
+                        </button>
+                    </>
+                    :
+                    <>
+                        <button onClick={() => setSearchParams({edit: true})} className={"btn btn-primary sms-btn"}>
+                            {LABELS_AND_HEADINGS.EDIT}
+                        </button>
+                    </>
+            }
         </div>
     )
 }
