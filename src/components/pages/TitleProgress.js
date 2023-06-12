3b203ce1aa61ff504bc12b50a4e23b2ca2499d6a
JoakimJohnsson <joakim.johnsson@su.se>
2023-06-12 12:14:07 +0200

    Add label to progress bar

--- src/components/pages/TitleProgress.js
+++ src/components/pages/TitleProgress.js
@@ -6,20 +6,21 @@ import {TEXTS} from "../../helpers/constants";
 export const TitleProgress = ({titleProgress}) => {
 
     let collectText100 = TEXTS.COLLECTING_TITLE_100;
-    let collectText = TEXTS.COLLECTING_TITLE_TEXT_1 + " " + titleProgress.progress + TEXTS.COLLECTING_TITLE_TEXT_2;
+    let collectTextProgress = TEXTS.COLLECTING_TITLE_TEXT_1 + " " + titleProgress.progress + TEXTS.COLLECTING_TITLE_TEXT_2;
+    let collectTextParts = titleProgress.noCollectedIssues + " / " + titleProgress.totalIssues;
 
     return (
         <div className={"mb-3"}>
             {
                 <>
                     <p>
-                        {titleProgress.progress === 100 ? collectText100 : collectText}
+                        {titleProgress.progress === 100 ? collectText100 : collectTextProgress}
                     </p>
                     {
                         titleProgress.progress === 100 ?
                             <ProgressBar striped variant="success" now={titleProgress.progress}/>
                             :
-                            <ProgressBar striped now={titleProgress.progress}/>
+                            <ProgressBar striped now={titleProgress.progress} label={collectTextParts}/>
 
                     }
                 </>
