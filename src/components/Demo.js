fe298a21560df72e9d1a3e9bfec955ecb06d05c3
JoakimJohnsson <joakim.johnsson@su.se>
2023-07-16 09:50:08 +0200

    Add admin edit button to publisher

--- src/components/Demo.js
+++ src/components/Demo.js
@@ -29,7 +29,7 @@ const Demo = () => {
                     <li className={"list-group-item px-0"}>Visa format på titelkort.</li>
                     <li className={"list-group-item px-0"}>Visa senast inlagda information på startsidan.</li>
                     <li className={"list-group-item px-0"}>Visa hur många titlar som återstår att lägga in i databasen.</li>
-                    <li className={"list-group-item px-0"}>Länk för redigering på titel och publikation - Bara synlig för admin.</li>
+                    <li className={"list-group-item px-0"}>Länk för redigering på titel, förlag och publikation - Bara synlig för admin.</li>
                 </ul>
                 <h3>{LABELS_AND_HEADINGS.RELEASE_PREVIOUS} v0.8.8 - 2023-06-15</h3>
                 <ul className={"list-group list-group-flush mb-4"}>
