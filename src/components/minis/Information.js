b505de36abab616996caf0cae6702484d58060a4
JoakimJohnsson <joakim.johnsson@su.se>
2023-07-20 19:54:20 +0200

    Fix - close information message after 15 seconds

--- src/components/minis/Information.js
+++ src/components/minis/Information.js
@@ -1,14 +1,14 @@
-import React, {useEffect, useState} from "react";
-import {ALERT_VARIANTS, MESSAGES} from "../../helpers/constants";
+import React, {useCallback, useEffect, useState} from "react";
+import {ALERT_VARIANTS, LABELS_AND_HEADINGS, MESSAGES} from "../../helpers/constants";
 import {useAppContext} from "../../context/AppContext";
-import {faTimes} from "@fortawesome/pro-regular-svg-icons";
+import {faTimes, faPause} from "@fortawesome/pro-regular-svg-icons";
 import {Icon} from "../icons";
 
 
 export const Information = ({message}) => {
-    const INFORMATION_MESSAGE_MAX_AGE = 15;
-    const [timeStamp, setTimeStamp] = useState("");
+    const INFORMATION_MESSAGE_MAX_AGE = 10;
     const [counter, setCounter] = useState(INFORMATION_MESSAGE_MAX_AGE);
+    const [pauseCounter, setPauseCounter] = useState(false);
     const [messageText, setMessageText] = useState("");
     const statusClass = message.status ? message.status.toString().charAt(0) : 4;
     const alertVariant = ALERT_VARIANTS[statusClass].variant;
@@ -16,18 +16,22 @@ export const Information = ({message}) => {
     const {setInformationMessage} = useAppContext();
     const doShow = message && message.show ? "show" : "";
 
-    useEffect(() => {
-        if (message && message.show) {
-            counter > 0 && setTimeout(() => setCounter(counter - 1), 1000);
-            if (counter === 0) {
-                setCounter(INFORMATION_MESSAGE_MAX_AGE);
-                setInformationMessage(null);
-            }
-        }
-    }, [counter, message, setInformationMessage]);
+    const closeInformationMessage = useCallback(() => {
+        setCounter(INFORMATION_MESSAGE_MAX_AGE);
+        setInformationMessage(null);
+        setPauseCounter(false);
+    }, [setInformationMessage]);
 
     useEffect(() => {
-        setTimeStamp(new Date().toLocaleTimeString())
+        if (message && message.show && !pauseCounter) {
+            counter > 0 && setTimeout(() => setCounter(counter - 1), 1000);
+            if (counter === 0) {
+                closeInformationMessage();
+            }
+        }
+    }, [counter, message, closeInformationMessage, pauseCounter]);
+
+    useEffect(() => {
         if (message.status && typeof message.error !== "string") {
             setMessageText(MESSAGES.CODES[message.status]);
         } else if (message.error) {
@@ -40,19 +44,30 @@ export const Information = ({message}) => {
     return message && message.show && (
         <div className={`alert alert-${alertVariant} alert-dismissible fade ${doShow} sms-information-message`} role={"alert"}>
             <div className={"d-flex"}>
-            <div>
-                {alertIcon}
+                <div>
+                    {alertIcon}
+                </div>
+                <div>
+                    {
+                        !pauseCounter &&
+                        <p className={"small d-block mb-1"}>{MESSAGES.INFO.WILL_CLOSE_1} <span className={"fs-6"}>{counter}</span> {MESSAGES.INFO.WILL_CLOSE_2}</p>
+                    }
+                    <p className={"mb-0"}>{messageText}</p>
+                </div>
             </div>
-            <div>
-                <time className={"small d-block mb-1"}>{timeStamp}</time>
-            <p className={"mb-0"}>{messageText} {MESSAGES.INFO.WILL_CLOSE_1} {counter} {MESSAGES.INFO.WILL_CLOSE_2} </p>
+            <div className={"position-absolute top-0 end-0"}>
+                {
+                    !pauseCounter &&
+                    <button type="button" className="p-1 btn sms-icon-btn text-black-50" aria-label={LABELS_AND_HEADINGS.PAUSE}
+                            onClick={() => setPauseCounter(!pauseCounter)}>
+                        <Icon icon={faPause} className={"fa-lg sms-icon--hovering"}/>
+                    </button>
+                }
+                <button type="button" className="p-1 btn sms-icon-btn text-black-50" aria-label={LABELS_AND_HEADINGS.CLOSE}
+                        onClick={() => closeInformationMessage()}>
+                    <Icon icon={faTimes} className={"fa-lg sms-icon--hovering"}/>
+                </button>
             </div>
-            </div>
-            <button type="button" className="position-absolute top-0 end-0 p-2 btn sms-icon-btn text-black-50" data-bs-dismiss="alert"
-                    aria-label="Stäng"
-                    onClick={() => setInformationMessage(MESSAGES.EMPTY)}>
-                <Icon icon={faTimes} className={"fa-lg sms-icon--hovering"}/>
-            </button>
         </div>
     )
 }
