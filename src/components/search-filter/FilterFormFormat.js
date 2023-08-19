2ec8d2e717b96687175f917091e7f3249461ad7f
JoakimJohnsson <joakim.johnsson@su.se>
2023-08-19 21:10:39 +0200

    Update packages - somewhat working POC for handling filtering

--- src/components/search-filter/FilterFormFormat.js
+++ src/components/search-filter/FilterFormFormat.js
@@ -1,9 +1,10 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faTimes} from "@fortawesome/pro-solid-svg-icons";
 import {faFilters} from "@fortawesome/pro-duotone-svg-icons";
 import {DEFAULT_SEARCH_PARAMS_FORMATS, LABELS_AND_HEADINGS} from "../../helpers/constants";
 import formatData from "../../helpers/valueLists/formats.json";
+import {isTrue} from "../../helpers/functions/functions";
 
 
 const FilterFormFormat = ({
@@ -27,8 +28,32 @@ const FilterFormFormat = ({
     const [filterHardcover, setFilterHardcover] = useState(false);
     const [filterSpecial, setFilterSpecial] = useState(false);
 
+    // Pick up search params if provided
+    useEffect(() => {
+        setFilterQuery(query || "");
+        setFilterComic(isTrue(comic) || false);
+        setFilterComiclarge(isTrue(comiclarge) || false);
+        setFilterAlbum(isTrue(album) || false);
+        setFilterPocket(isTrue(pocket) || false);
+        setFilterHardcover(isTrue(hardcover) || false);
+        setFilterSpecial(isTrue(special) || false);
+    }, [query, comic, comiclarge, album, pocket, hardcover, special]);
+
+    const updateSearchParams = (e) => {
+        e.preventDefault();
+        setSearchParams({
+            query: filterQuery,
+            comic: filterComic,
+            comiclarge: filterComiclarge,
+            album: filterAlbum,
+            pocket: filterPocket,
+            hardcover: filterHardcover,
+            special: filterSpecial
+        });
+    }
+
     return (
-        <div className={"form-group sms-section--light mb-4"}>
+        <form className={"form-group sms-section--light mb-4"} onSubmit={(e) => updateSearchParams(e)}>
             <div className="col-12 mb-2">
                 <label className={"form-label"} htmlFor="query">{LABELS_AND_HEADINGS.DO_FILTER}</label>
                 <div className="input-group mb-3">
@@ -38,15 +63,15 @@ const FilterFormFormat = ({
                            type="text"
                            className="form-control border-bottom-0"
                            placeholder={placeholder}
-                           value={query}
-                           onChange={e => setSearchParams({query: e.target.value})}
+                           value={filterQuery}
+                           onChange={(e) => setFilterQuery(e.target.value)}
                     />
                 </div>
                 <div className={"input-group mb-3"}>
                     {/* comic */}
                     <div className={"form-check p-0 me-3"}>
-                        <input type={"checkbox"} className={"btn-check"} id={formatData[0].id} autoComplete="off"/>
-                        <label className={"btn btn-sm btn-outline-secondary mb-2"} htmlFor={formatData[0].id}>{formatData[0].name}</label>
+                        <input type={"checkbox"} className={"btn-check"} id={formatData[0].id} autoComplete="off" onChange={() => setFilterComic(!filterComic)}/>
+                        <label className={`btn btn-sm ${filterComic ? "btn-secondary" : "btn-primary"} mb-2`} htmlFor={formatData[0].id}>{formatData[0].name}</label>
                     </div>
                     {/* comiclarge */}
                     <div className={"form-check p-0 me-3"}>
@@ -81,8 +106,14 @@ const FilterFormFormat = ({
                         {LABELS_AND_HEADINGS.RESET_FILTER}
                     </button>
                 }
+                <button className="btn btn-primary" type={"submit"}>
+                    <FontAwesomeIcon icon={faTimes} className={"me-2"}/>
+                    Submit
+                </button>
+                <p>filterQuery: {filterQuery}</p>
+                <p>filterComic: {filterComic.toString()}</p>
             </div>
-        </div>
+        </form>
     )
 };
 export default FilterFormFormat;
