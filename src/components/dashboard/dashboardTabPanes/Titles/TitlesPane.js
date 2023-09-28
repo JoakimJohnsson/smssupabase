e2df814e1eef46110a332d7a0c0f0123cb1f107a
JoakimJohnsson <joakim.johnsson@su.se>
2023-09-28 09:11:43 +0200

    Refactor titles pane

--- src/components/dashboard/dashboardTabPanes/Titles/TitlesPane.js
+++ src/components/dashboard/dashboardTabPanes/Titles/TitlesPane.js
@@ -5,8 +5,7 @@ import {getTitlesForUser} from "../../../../helpers/functions/serviceFunctions/t
 import {CustomSpinner} from "../../../minis/CustomSpinner";
 import FilterFormFormat from "../../../search-filter/FilterFormFormat";
 import {useFormatQueryFilter} from "../../../../helpers/customHooks/useFormatQueryFilter";
-import {filterByFormat, filterQueryByNameAndStartYear, hasTrueValue, sortByNameAndStartYear} from "../../../../helpers/functions/functions";
-import {Link} from "react-router-dom";
+import {TitlesPaneList} from "./TitlesPaneList";
 
 
 export const TitlesPane = () => {
@@ -37,69 +36,8 @@ export const TitlesPane = () => {
                 loading ?
                     <CustomSpinner size={"4x"}/>
                     :
-                    <ul className={"sms-list--with-cards"}>
-                        {
-                            query ?
-                                titlesData
-                                    .filter((title) => {
-                                        return (
-                                            filterQueryByNameAndStartYear(title, query)
-                                        )
-                                    })
-                                    .filter((title) => {
-                                        if (hasTrueValue([comic, comiclarge, album, pocket, hardcover, special])) {
-                                            return (
-                                                filterByFormat(title, comic, comiclarge, album, pocket, hardcover, special)
-                                            )
-                                        } else {
-                                            return true;
-                                        }
-                                    })
-                                    .sort((a, b) => sortByNameAndStartYear(a, b))
-                                    .map((t) =>
-                                        <li key={t.id} className={"title-card"}>
-                                            <Link to={`/titles/${t.id}`} className={"hocus-standard"}
-                                                  title={t.name}>
-                                                <div className={"image-container mb-2 position-relative"}>
-                                                    <img
-                                                        src={t.image_url}
-                                                        alt={t.name}
-                                                        className="w-100"
-                                                        loading={"lazy"}
-                                                    />
-                                                </div>
-                                            </Link>
-                                        </li>
-                                    )
-                                :
-                                titlesData
-                                    .filter((title) => {
-                                        if (hasTrueValue([comic, comiclarge, album, pocket, hardcover, special])) {
-                                            return (
-                                                filterByFormat(title, comic, comiclarge, album, pocket, hardcover, special)
-                                            )
-                                        } else {
-                                            return true;
-                                        }
-                                    })
-                                    .sort((a, b) => sortByNameAndStartYear(a, b))
-                                    .map((t) =>
-                                        <li key={t.id} className={"title-card"}>
-                                            <Link to={`/titles/${t.id}`} className={"hocus-standard"}
-                                                  title={t.name}>
-                                                <div className={"image-container mb-2 position-relative"}>
-                                                    <img
-                                                        src={t.image_url}
-                                                        alt={t.name}
-                                                        className="w-100"
-                                                        loading={"lazy"}
-                                                    />
-                                                </div>
-                                            </Link>
-                                        </li>
-                                    )
-                        }
-                    </ul>
+                    <TitlesPaneList query={query} titlesData={titlesData} comic={comic} comiclarge={comiclarge} album={album} pocket={pocket}
+                                    hardcover={hardcover} special={special}/>
 
             }
         </>
---
+++ src/components/dashboard/dashboardTabPanes/Titles/TitlesPaneList.js
@@ -0,0 +1,73 @@
+import React from "react";
+import {filterByFormat, filterQueryByNameAndStartYear, hasTrueValue, sortByNameAndStartYear} from "../../../../helpers/functions/functions";
+import {Link} from "react-router-dom";
+
+
+export const TitlesPaneList = ({query, titlesData, comic, comiclarge, album, pocket, hardcover, special}) => {
+
+    return (
+        <ul className={"sms-list--with-cards"}>
+            {
+                query ?
+                    titlesData
+                        .filter((title) => {
+                            return (
+                                filterQueryByNameAndStartYear(title, query)
+                            )
+                        })
+                        .filter((title) => {
+                            if (hasTrueValue([comic, comiclarge, album, pocket, hardcover, special])) {
+                                return (
+                                    filterByFormat(title, comic, comiclarge, album, pocket, hardcover, special)
+                                )
+                            } else {
+                                return true;
+                            }
+                        })
+                        .sort((a, b) => sortByNameAndStartYear(a, b))
+                        .map((t) =>
+                            <li key={t.id} className={"title-card"}>
+                                <Link to={`/titles/${t.id}`} className={"hocus-standard"}
+                                      title={t.name}>
+                                    <div className={"image-container mb-2 position-relative"}>
+                                        <img
+                                            src={t.image_url}
+                                            alt={t.name}
+                                            className="w-100"
+                                            loading={"lazy"}
+                                        />
+                                    </div>
+                                </Link>
+                            </li>
+                        )
+                    :
+                    titlesData
+                        .filter((title) => {
+                            if (hasTrueValue([comic, comiclarge, album, pocket, hardcover, special])) {
+                                return (
+                                    filterByFormat(title, comic, comiclarge, album, pocket, hardcover, special)
+                                )
+                            } else {
+                                return true;
+                            }
+                        })
+                        .sort((a, b) => sortByNameAndStartYear(a, b))
+                        .map((t) =>
+                            <li key={t.id} className={"title-card"}>
+                                <Link to={`/titles/${t.id}`} className={"hocus-standard"}
+                                      title={t.name}>
+                                    <div className={"image-container mb-2 position-relative"}>
+                                        <img
+                                            src={t.image_url}
+                                            alt={t.name}
+                                            className="w-100"
+                                            loading={"lazy"}
+                                        />
+                                    </div>
+                                </Link>
+                            </li>
+                        )
+            }
+        </ul>
+    )
+}
