bf687275fa3f4c19c467989aaa653583038ccfef
JoakimJohnsson <joakim.johnsson@su.se>
2023-10-07 11:33:06 +0200

    Minor fix

--- src/components/Demo.js
+++ src/components/Demo.js
@@ -23,7 +23,7 @@ const Demo = () => {
             <div>
                 <h2 className={"text-center"}>{LABELS_AND_HEADINGS.DEMO}</h2>
                 <p className={"lead"}>{TEXTS.DEMO_TEXT_1}</p>
-                <h3>{LABELS_AND_HEADINGS.RELEASE_LATEST} v0.9.3 - 2023-09-xx</h3>
+                <h3>{LABELS_AND_HEADINGS.RELEASE_LATEST} v0.9.3 - 2023-10-07</h3>
                 <ul className={"list-group list-group-flush mb-4"}>
                     <li className={"list-group-item px-0"}>Blandade småfixar och förbättringar.</li>
                     <li className={"list-group-item px-0"}>Lägga in och visa information om källmaterial.</li>
