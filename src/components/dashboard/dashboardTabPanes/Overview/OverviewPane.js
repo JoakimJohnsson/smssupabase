1eda68b78c9342586101eb21fb3706c17c8f1e64
JoakimJohnsson <joakim.johnsson@su.se>
2023-10-26 13:59:06 +0200

    Cleanup before beta release

--- src/components/dashboard/dashboardTabPanes/Overview/OverviewPane.js
+++ src/components/dashboard/dashboardTabPanes/Overview/OverviewPane.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from "react";
-import {PANES, STATISTICS, TABLES} from "../../../../helpers/constants";
+import {PANES, TABLES} from "../../../../helpers/constants";
 import {useAppContext} from "../../../../context/AppContext";
-import {getRowCountByTableAndUserId} from "../../../../helpers/functions/serviceFunctions/serviceFunctions";
+import {getCountByTable, getRowCountByTableAndUserId} from "../../../../helpers/functions/serviceFunctions/serviceFunctions";
 import {CustomSpinner} from "../../../minis/CustomSpinner";
 import {getTitlesForUser, getTotalIssuesCountForTitlesData} from "../../../../helpers/functions/serviceFunctions/titleFunctions";
 
@@ -12,6 +12,7 @@ export const OverviewPane = () => {
     const [userTitlesData, setUserTitlesData] = useState(null);
     const [totalIssuesCountForCollection, setTotalIssuesCountForCollection] = useState(null);
     const [userIssuesCount, setUserIssuesCount] = useState(null);
+    const [totalTitles, setTotalTitles] = useState(0);
 
     useEffect(() => {
         if (user) {
@@ -35,13 +36,17 @@ export const OverviewPane = () => {
         }
     }, [userTitlesData]);
 
+    useEffect(() => {
+        getCountByTable(TABLES.TITLES, setTotalTitles).then();
+    }, []);
+
     return (
         <div>
             <h1>{PANES.OVERVIEW.NAME}</h1>
             {
                 userTitlesData ?
                     <p>
-                        {PANES.OVERVIEW.COLLECTING_TITLES_1} {userTitlesData.length} {PANES.OVERVIEW.COLLECTING_TITLES_2} {STATISTICS.TOTAL_TITLES_COUNT} {PANES.OVERVIEW.COLLECTING_TITLES_3}
+                        {PANES.OVERVIEW.COLLECTING_TITLES_1} {userTitlesData.length} {PANES.OVERVIEW.COLLECTING_TITLES_2} {totalTitles} {PANES.OVERVIEW.COLLECTING_TITLES_3}
                     </p>
                     :
                     <CustomSpinner className={"mb-3 d-block"}/>
