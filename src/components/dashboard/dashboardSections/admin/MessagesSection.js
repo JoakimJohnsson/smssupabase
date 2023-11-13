82d9e9dfca42f279755c7bc4baad89fe817bab62
JoakimJohnsson <joakim.johnsson@su.se>
2023-11-13 13:40:14 +0100

    Adjust col width

--- src/components/dashboard/dashboardSections/admin/MessagesSection.js
+++ src/components/dashboard/dashboardSections/admin/MessagesSection.js
@@ -5,7 +5,7 @@ import {LABELS_AND_HEADINGS} from "../../../../helpers/constants";
 export const MessagesSection = () => {
 
     return (
-        <div className={"sms-page-col mb-5 "}>
+        <div className={"sms-page-col--full mb-5 "}>
             <div className={"sms-section--light"}>
                 <h2>{LABELS_AND_HEADINGS.MESSAGES}</h2>
             </div>
