275eb923ed4ede590c431a2881fd6d2bf37398dd
JoakimJohnsson <joakim.johnsson@su.se>
2023-11-26 13:02:02 +0100

    WIP - Refactoring status icons

--- src/components/lists/messages/TopicIcon.js
+++ src/components/lists/messages/TopicIcon.js
@@ -1,10 +1,10 @@
 import React from "react";
 import topicData from "../../../helpers/valueLists/topics.json";
 import {getIconByName, Icon} from "../../icons";
-import {getDataIcon} from "../../../helpers/functions/functions";
+import {getDataIcon, getDataShade} from "../../../helpers/functions/functions";
 
 export const TopicIcon = ({topicId}) => {
     return topicId && (
-        <Icon icon={getIconByName(getDataIcon(topicData, topicId))} className={"me-2 text-secondary fa-fw"}/>
+        <Icon icon={getIconByName(getDataIcon(topicData, topicId))} className={`me-2 text-grade-${getDataShade(topicData, topicId)} fa-fw`}/>
     )
 }
