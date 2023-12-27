6996b2f4a7880db62deb36a8e99d7c97dde46d91
JoakimJohnsson <joakim.johnsson@su.se>
2023-12-27 14:04:16 +0100

    Edit release notes

--- src/components/Information.js
+++ src/components/Information.js
@@ -17,7 +17,7 @@ const Information = () => {
                     <h2 className={"text-center"}>{LABELS_AND_HEADINGS.INFORMATION}</h2>
                     <p className={"lead"}>{TEXTS.INFO_TEXT_1}</p>
                 </div>
-                <h3>{LABELS_AND_HEADINGS.RELEASE_LATEST} v1.3.0 - 2024-XX-XX</h3>
+                <h3>{LABELS_AND_HEADINGS.RELEASE_LATEST} v1.3.0 - 2023-12-27</h3>
                 <ul className={"list-group list-group-flush mb-4"}>
                     <li className={"list-group-item px-0"}>Blandade småfixar och förbättringar.</li>
                     <li className={"list-group-item px-0"}>Visar nu antal träffar när man söker/filtrerar bland "Alla titlar".</li>
