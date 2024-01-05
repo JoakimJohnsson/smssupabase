a58abb160faddc1f5908ac11e837935543466581
JoakimJohnsson <joakim.johnsson@su.se>
2024-01-05 11:12:04 +0100

    Use overlay spinner

--- src/components/pages/admin/users/AdminUsers.js
+++ src/components/pages/admin/users/AdminUsers.js
@@ -4,7 +4,6 @@ import {getRowsByTable} from "../../../../services/serviceFunctions";
 import {useNavigate} from "react-router-dom";
 import {Breadcrumbs} from "../../../minis/Breadcrumbs";
 import {handleBacking} from "../../../../helpers/functions";
-import {NoDataAvailable} from "../../../minis/NoDataAvailable";
 import {faArrowLeft} from "@fortawesome/pro-regular-svg-icons";
 import {IconButton} from "../../../minis/IconButton";
 import {UsersList} from "../../../lists/users/UsersList";
