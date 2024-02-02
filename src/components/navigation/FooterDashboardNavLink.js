36a610c85e121134d5813154417f87a96d51c905
JoakimJohnsson <joakim.johnsson@su.se>
2024-02-02 16:24:57 +0100

    Beautiful nav links

--- src/components/navigation/FooterDashboardNavLink.js
+++ src/components/navigation/FooterDashboardNavLink.js
@@ -5,7 +5,7 @@ import {NavLink} from "react-router-dom";
 export const FooterDashboardNavLink = ({route, variant, icon, text}) => {
     const className = "nav-item nav-link";
     return (
-        <NavLink to={route} className={className + " " + variant}>
+        <NavLink to={route} className={className + " " + variant} title={text}>
             <div className={"d-flex align-items-center justify-content-center flex-column flex-sm-column flex-lg-row w-100"}>
                 {icon}
                 <span className={"footer-navlink-text"}>{text}</span>
