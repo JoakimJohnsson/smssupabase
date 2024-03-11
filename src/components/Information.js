3fdd41798ea55f2cef1b2989c3dbcdb9a619c47b
JoakimJohnsson <joakim.johnsson@su.se>
2024-03-11 17:26:24 +0100

    Update changelog

--- src/components/Information.js
+++ src/components/Information.js
@@ -16,7 +16,7 @@ const Information = () => {
                     <h2 className={"text-center"}>{LABELS_AND_HEADINGS.INFORMATION}</h2>
                     <p className={"lead"}>{TEXTS.INFO_TEXT_1}</p>
                 </div>
-                <h3>{LABELS_AND_HEADINGS.RELEASE_LATEST} v1.6.3 - 2024-03-XX</h3>
+                <h3>{LABELS_AND_HEADINGS.RELEASE_LATEST} v1.6.3 - 2024-03-11</h3>
                 <ul className={"list-group list-group-flush mb-4"}>
                     <li className={"list-group-item px-0"}>Blandade småfixar och förbättringar.</li>
                     <li className={"list-group-item px-0"}>Nu visas senast inlagda titlar och publikationer korrekt på startsidan.</li>
