c6cda8c960ac14261f24f3cd77ec4612ebeb2d0d
JoakimJohnsson <joakim.johnsson@su.se>
2024-11-05 07:31:02 +0100

    Prepare release notes

--- src/components/pages/home/Information.jsx
+++ src/components/pages/home/Information.jsx
@@ -42,6 +42,7 @@ const Information = () => {
                     <li className={"list-group-item px-0"}>Ytterligare förbättringar av publikationsväljaren.</li>
                     <li className={"list-group-item px-0"}>Möjlighet att lägga till sina favorittitlar och favoritpublikationer.</li>
                     <li className={"list-group-item px-0"}>Förbättrad UX för funktionsknappar på titel och publikation.</li>
+                    <li className={"list-group-item px-0"}>Prestandaförbättringar för databasen - minskat antal anrop och kortare svarstider.</li>
                 </ul>
                 <h3>{LABELS.COMMON.RELEASE_PREVIOUS}</h3>
                 <ul className={"list-group list-group-flush mb-4"}>
