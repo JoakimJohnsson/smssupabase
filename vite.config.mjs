b57d5e18566d015394f6334d017e66206bf47976
JoakimJohnsson <joakim.johnsson@su.se>
2024-11-19 18:43:40 +0100

    Implement rollup chunks

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,28 +1,60 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import jsconfigPaths from 'vite-jsconfig-paths';
+import {defineConfig} from "vite";
+import react from "@vitejs/plugin-react";
+import jsconfigPaths from "vite-jsconfig-paths";
 import eslint from "vite-plugin-eslint";
 
-export default defineConfig ({
 
+export default defineConfig({
     plugins: [react(), jsconfigPaths(), eslint()],
     server: {
         port: 3000,
-        open: '/'
+        open: "/"
     },
     preview: {
         port: 3000
     },
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id) { // The id parameter represents the path to each module/file.
+                    // Group React libraries
+                    if (id.includes('react')) {
+                        return 'react';
+                    }
+
+                    // Group other libraries
+                    if (id.includes('@supabase/supabase-js') ||
+                        id.includes('@vis.gl/react-google-maps')) {
+                        return 'other';
+                    }
+
+                    // Group FontAwesome libraries
+                    if (id.includes('@fortawesome')) {
+                        return 'fontawesome';
+                    }
+
+                    // Create a chunk for src/components folder
+                    if (id.includes('/src/components/')) {
+                        const match = id.match(/\/src\/components\/([^/]+)/); // Match subfolder or file
+                        if (match) {
+                            return `components-${match[1]}`; // Create chunks
+                        }
+                        return 'components'; // Default for files directly in /components/
+                    }
+                },
+            },
+        }
+    },
     test: {
         globals: true,
-        environment: 'jsdom',
-        setupFiles: 'src/tests/setupTests.js',
-        include: ['src/tests/**/*.{test,spec}.{js,jsx,ts,tsx}'] // Only run tests in this folder
+        environment: "jsdom",
+        setupFiles: "src/tests/setupTests.js",
+        include: ["src/tests/**/*.{test,spec}.{js,jsx,ts,tsx}"] // Only run tests in this folder
     },
     css: {
         preprocessorOptions: {
             scss: {
-                api: 'modern-compiler',
+                api: "modern-compiler",
             }
         }
     }
