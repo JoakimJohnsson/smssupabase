5d7db95f73a4409654e1e4d1f20b40676324b0fb
JoakimJohnsson <joakim.johnsson@su.se>
2024-12-18 12:12:27 +0100

    WIP - Hero header fix

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,40 +1,18 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import {useAppContext} from "../../context/AppContext";
 import {AuthorizedNavigation} from "../navigation/AuthorizedNavigation";
 import {Information} from "../minis/Information";
 import {SkipLink} from "../pages/pagecomponents/SkipLink";
-import {HeroHeader} from "./HeroHeader.jsx";
-import {useLocation} from "react-router-dom";
-import {CONFIG, ROUTES} from "../../helpers/constants/configConstants.jsx";
 
 
 export const Header = () => {
 
     const {informationMessage, user, profile} = useAppContext();
-    const location = useLocation();
-    const [doShowHeroHeader, setDoShowHeroHeader] = useState(false);
 
-    // Define routes where HeroHeader should NOT be displayed
-    const disallowedHeroHeaderRoutes = [ROUTES.SUCCESS, ROUTES.CHANGE_PASSWORD];
-
-    useEffect(() => {
-            if(!user && !profile && !disallowedHeroHeaderRoutes.includes(location.pathname)) {
-                // Update messages after a while.
-                const timer = setTimeout(() => {
-                    setDoShowHeroHeader(true);
-                }, CONFIG.TIMEOUT_XXL);
-                return () => clearTimeout(timer);
-            }
-    }, []);
-
-    return user && profile ?
+    return user && profile &&
         <header className={"sms-header"}>
             <SkipLink/>
             <AuthorizedNavigation/>
             <Information message={informationMessage}/>
         </header>
-        :
-        (
-            doShowHeroHeader && <HeroHeader/>
-        )
 };
