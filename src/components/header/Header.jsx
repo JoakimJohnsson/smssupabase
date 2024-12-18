2846aa503c43a480a7c179d3970953f45504057d
JoakimJohnsson <joakim.johnsson@su.se>
2024-12-18 06:29:59 +0100

    WIP - refactor page layouts

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,22 +1,31 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {useAppContext} from "../../context/AppContext";
 import {AuthorizedNavigation} from "../navigation/AuthorizedNavigation";
 import {Information} from "../minis/Information";
 import {SkipLink} from "../pages/pagecomponents/SkipLink";
 import {HeroHeader} from "./HeroHeader.jsx";
 import {useLocation} from "react-router-dom";
-import {ROUTES} from "../../helpers/constants/configConstants.jsx";
+import {CONFIG, ROUTES} from "../../helpers/constants/configConstants.jsx";
 
 
 export const Header = () => {
 
     const {informationMessage, user, profile} = useAppContext();
     const location = useLocation();
+    const [doShowHeroHeader, setDoShowHeroHeader] = useState(false);
 
     // Define routes where HeroHeader should NOT be displayed
     const disallowedHeroHeaderRoutes = [ROUTES.SUCCESS, ROUTES.CHANGE_PASSWORD];
 
-    const showHeroHeader = !user && !profile && !disallowedHeroHeaderRoutes.includes(location.pathname);
+    useEffect(() => {
+            if(!user && !profile && !disallowedHeroHeaderRoutes.includes(location.pathname)) {
+                // Update messages after a while.
+                const timer = setTimeout(() => {
+                    setDoShowHeroHeader(true);
+                }, CONFIG.TIMEOUT_XXL);
+                return () => clearTimeout(timer);
+            }
+    }, []);
 
     return user && profile ?
         <header className={"sms-header"}>
@@ -26,6 +35,6 @@ export const Header = () => {
         </header>
         :
         (
-            showHeroHeader && <HeroHeader/>
+            doShowHeroHeader && <HeroHeader/>
         )
 };
